f155cdc92c8ce3072abd24d6bd8efc20c35387a5
MrAiken007 <jorgepaim2005@gmail.com>
2025-02-08 19:40:03 +0100

    New

--- sustainable_api/sustainable_api/settings.py
+++ sustainable_api/sustainable_api/settings.py
@@ -1,5 +1,9 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv # type: ignore
+from urllib.parse import urlparse
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,18 +12,15 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
-    # Apps padrão do Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Apps de terceiros
     'rest_framework',
     'drf_yasg',
-    # Nossa app com configuração customizada
-    'acoes.apps.AcoesConfig',
+    'acoes',
 ]
 
 MIDDLEWARE = [
@@ -53,17 +54,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sustainable_api.wsgi.application'
 
 # Configuração do PostgreSQL – substitua pelos dados do seu banco
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'nome_do_banco',
-        'USER': 'seu_usuario',
-        'PASSWORD': 'sua_senha',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
     }
 }
-
 AUTH_PASSWORD_VALIDATORS = [
     { 'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', },
     { 'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator', },
